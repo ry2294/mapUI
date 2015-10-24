4e8386dd69b0026db7b1f40536c131a2f131b351
Rakesh <ry2294@columbia.edu>
2015-10-24 14:59:54 -0400

    Fetching data from dynamodb

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path    = require("path");
+
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+    accessKeyId: 'AKIAIVVCT34IQL2IWDKA', 
+    secretAccessKey: '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'});
+
+AWS.config.update({region: 'us-west-2'});
+
+var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
+var dynamodb = new AWS.DynamoDB();
+
+var getPlaceInfoParams = {
+    TableName: "place_tbl"
+};
+
+dynamodb.scan(getPlaceInfoParams, function(error, data) {
+    if (error)
+        console.log(JSON.stringify(error));
+    else
+        console.log(JSON.stringify(data));
+});
+
+
+var app = express();
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+router.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.use('/', router);
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
